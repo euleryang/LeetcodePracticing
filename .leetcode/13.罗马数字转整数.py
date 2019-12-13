a5d3fc096eb91405d8659c75e5c8d8fe2e1ac65e
root <huan_yang2006@163.com>
2019-12-13 20:46:27 +0800

    romanToInt

---
+++ .leetcode/13.罗马数字转整数.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=13 lang=python3
+#
+# [13] 罗马数字转整数
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic = {'I':1, 'IV':3, 'V':5, 'IX':8, 'X':10, 'XL':30, 'L':50, 'XC':80, 'C':100, 'CD':300, 'D':500, 'CM':800, 'M':1000}
+        return sum(dic.get(s[max(i-1, 0):i+1], dic[n]) for i, n in enumerate(s))
+# @lc code=end
+
