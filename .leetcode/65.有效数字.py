4aba2af9a1b1eedadb265d6a3619bca6f5d4efe0
root <huan_yang2006@163.com>
2019-12-15 19:46:10 +0800

    isNumber

---
+++ .leetcode/65.有效数字.py
@@ -0,0 +1,77 @@
+# coding:utf-8
+# @lc app=leetcode.cn id=65 lang=python3
+#
+# [65] 有效数字
+#
+# https://leetcode-cn.com/problems/valid-number/description/
+#
+# algorithms
+# Hard (17.05%)
+# Likes:    67
+# Dislikes: 0
+# Total Accepted:    7.3K
+# Total Submissions: 42.5K
+# Testcase Example:  '"0"'
+#
+# 验证给定的字符串是否可以解释为十进制数字。
+# 
+# 例如:
+# 
+# "0" => true
+# " 0.1 " => true
+# "abc" => false
+# "1 a" => false
+# "2e10" => true
+# " -90e3   " => true
+# " 1e" => false
+# "e3" => false
+# " 6e-1" => true
+# " 99e2.5 " => false
+# "53.5e93" => true
+# " --6 " => false
+# "-+3" => false
+# "95a54e53" => false
+# 
+# 说明: 我们有意将问题陈述地比较模糊。在实现代码之前，你应当事先思考所有可能的情况。这里给出一份可能存在于有效十进制数字中的字符列表：
+# 
+# 
+# 数字 0-9
+# 指数 - "e"
+# 正/负号 - "+"/"-"
+# 小数点 - "."
+# 
+# 
+# 当然，在输入中，这些字符的上下文也很重要。
+# 
+# 更新于 2015-02-10:
+# C++函数的形式已经更新了。如果你仍然看见你的函数接收 const char * 类型的参数，请点击重载按钮重置你的代码。
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        states = [
+            { 'b': 0, 's': 1, 'd': 2, '.': 4 }, # 0. start
+            { 'd': 2, '.': 4 } ,                # 1. 'sign' before 'e'
+            { 'd': 2, '.': 3, 'e': 5, 'b': 8 }, # 2. 'digit' before 'dot'
+            { 'd': 3, 'e': 5, 'b': 8 },         # 3. 'dot' with 'digit'
+            { 'd': 3 },                         # 4. no 'digit' before 'dot'
+            { 's': 6, 'd': 7 },                 # 5. 'e'
+            { 'd': 7 },                         # 6. 'sign' after 'e'
+            { 'd': 7, 'b': 8 },                 # 7. 'digit' after 'e'
+            { 'b': 8 }                          # 8. end with
+        ]
+        p = 0
+        for c in s:
+            if '0' <= c <= '9': typ = 'd'
+            elif c == ' ': typ = 'b'
+            elif c == '.': typ = '.'
+            elif c == 'e': typ = 'e'
+            elif c in "+-": typ = 's'
+            else: typ = '?'
+            if typ not in states[p]: return False
+            p = states[p][typ]
+        return p in [2, 3, 7, 8]     
+# @lc code=end
+
