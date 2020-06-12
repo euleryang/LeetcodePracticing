21c6164a3a9634955061cf280e8f5553d173fbb9
SamYang <huan_yang2006@qq.com>
2020-06-12 10:35:34 +0800

    27-removeElement-pylint

--- .leetcode/27.移除元素.py
+++ .leetcode/27.移除元素.py
@@ -6,7 +6,7 @@
 
 # @lc code=start
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement(self, nums, val) -> int:
         for i in range(len(nums)-1, -1, -1):
             if(nums[i] == val):
                 nums.pop(i)
